13ad89112a10eab0594a0e20b262def1e1cca0d9
eslam <eslamradii@gmail.com>
2022-11-19 04:27:50 +0200

    set app default endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Helpers\JsonResponse;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (JsonResponse $response) {
+    return $response->success(['app' => config('app.name')]);
 });
